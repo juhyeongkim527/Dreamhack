d84ec56d457a57dc2279d2fbf88182eafe5bf2e7
Juhyeong Kim <kesnt8892@gmail.com>
2024-08-22 15:37:20 +0000

    Double Free Bug

---
+++ study/Double_Free_Bug/dfb.c
@@ -0,0 +1,16 @@
+// Name: dfb.c
+// Compile: gcc -o dfb dfb.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *chunk;
+    chunk = malloc(0x50);
+
+    printf("Address of chunk: %p\n", chunk);
+
+    free(chunk);
+    free(chunk); // Free again
+}
