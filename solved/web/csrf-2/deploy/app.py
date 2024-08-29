58ebc72496f9caf98a2037e2054c91dd48ee78cc
Juhyeong Kim <kesnt8892@gmail.com>
2024-08-29 09:22:32 +0000

    csrf-2 solved

--- solved/web/csrf-2/deploy/app.py
+++ solved/web/csrf-2/deploy/app.py
@@ -67,24 +67,25 @@ def index():
 
 @app.route("/vuln")
 def vuln():
-    param = request.args.get("param", "").lower()
-    xss_filter = ["frame", "script", "on"]
+    param = request.args.get("param", "").lower()  # 이용자가 입력한 param 파라미터를 소문자로 변경
+    xss_filter = ["frame", "script", "on"]  # 세 가지 필터링 키워드
     for _ in xss_filter:
-        param = param.replace(_, "*")
+        param = param.replace(_, "*")  # 이용자가 입력한 값 중에 필터링 키워드가 있는 경우, '*'로 치환
     return param
 
 
-@app.route("/flag", methods=["GET", "POST"])
+@app.route("/flag", methods=["GET", "POST"])  # flag 페이지 라우팅 (GET, POST 요청을 모두 받음)
 def flag():
     if request.method == "GET":
         return render_template("flag.html")
     elif request.method == "POST":
         param = request.form.get("param", "")
-        session_id = os.urandom(16).hex()
-        session_storage[session_id] = 'admin'
-        if not check_csrf(param, {"name": "sessionid", "value": session_id}):
-            return '<script>alert("wrong??");history.go(-1);</script>'
+        session_id = os.urandom(16).hex()  # 무작위 세션 ID 생성 후 16진수 문자열로 변환
 
+        session_storage[session_id] = 'admin'  # 세션 ID를 키로 사용하여 'admin' 값을 session_storage 딕셔너리에 저장
+
+        if not check_csrf(param, {"name": "sessionid", "value": session_id}):  # CSRF 토큰 (세션 ID)이 유효한지 확인
+            return '<script>alert("wrong??");history.go(-1);</script>'
         return '<script>alert("good");history.go(-1);</script>'
 
 
@@ -92,20 +93,24 @@ def flag():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+
     elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+        username = request.form.get('username')  # POST 요청의 form 데이터에서 'username'을 가져옴
+        password = request.form.get('password')  # POST 요청의 form 데이터에서 'password'를 가져옴
+
         try:
             pw = users[username]
         except:
-            return '<script>alert("not found user");history.go(-1);</script>'
+            return '<script>alert("not found user");history.go(-1);</script>'  # 사용자가 존재하지 않는 경우 경고를 표시하고 이전 페이지로 이동
+
         if pw == password:
             resp = make_response(redirect(url_for('index')))
-            session_id = os.urandom(8).hex()
-            session_storage[session_id] = username
-            resp.set_cookie('sessionid', session_id)
-            return resp
-        return '<script>alert("wrong password");history.go(-1);</script>'
+            session_id = os.urandom(8).hex()  # 무작위 세션 ID를 생성하고 16진수 문자열로 변환
+            session_storage[session_id] = username  # 세션 ID를 키로 사용하여 현재 사용자를 'session_storage'에 저장
+            resp.set_cookie('sessionid', session_id)  # 생성된 세션 ID를 쿠키로 설정하여 사용자에게 전달
+            return resp    # 로그인이 성공한 경우 리디렉션 응답을 반환
+
+        return '<script>alert("wrong password");history.go(-1);</script>'  # 비밀번호가 일치하지 않는 경우 경고를 표시 후 이전 페이지로 이동
 
 
 @app.route("/change_password")
@@ -115,9 +120,9 @@ def change_password():
     try:
         username = session_storage[session_id]
     except KeyError:
-        return render_template('index.html', text='please login')
+        return render_template('index.html', text='please login')  # 세션 ID가 유효하지 않거나 세션을 찾을 수 없는 경우
 
-    users[username] = pw
+    users[username] = pw  # 세션에 연결된 사용자의 비밀번호를 'pw'로 변경
     return 'Done'
 
 
