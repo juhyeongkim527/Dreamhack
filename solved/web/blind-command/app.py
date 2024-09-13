b0b232460576b77d1e26a7954642ae264464de43
Juhyeong Kim <kesnt8892@gmail.com>
2024-09-13 21:24:03 +0000

    blind-command solved

--- solved/web/blind-command/app.py
+++ solved/web/blind-command/app.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/' , methods=['GET'])
+
+@app.route('/', methods=['GET'])  # GET 만 등록되어있는데, 어떻게 else에서 다른 메소드로 요청이 오는지?
 def index():
     cmd = request.args.get('cmd', '')
     if not cmd:
@@ -15,5 +16,6 @@ def index():
     else:
         os.system(cmd)
     return cmd
+
 
 app.run(host='0.0.0.0', port=8000)
